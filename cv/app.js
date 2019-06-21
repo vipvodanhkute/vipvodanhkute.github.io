17f974c7c283152ea83ad626e7a53a5985b93bcb
unknown <vipvodanhmaster@gmail.com>
2019-06-21 22:19:05 +0700

    up c5

--- cv/app.js
+++ cv/app.js
@@ -13,30 +13,30 @@ function c2(e) {
     e.preventDefault();
     var c2a = $("#c2a").val().split("\n");
     n = c2a.length;
-    if(c2a[0]!=n-1){
+    if (c2a[0] != n - 1) {
         $("#c2b").html("Du lieu khong dung")
-    }else{
-    let arr = [];
-    count = 1;
-    for (let i = 1; i < n; i++) {
-        arr[i] = [];
-        for (let j = 0; j < c2a[i].length; j++) {
-            if (c2a[i].charAt(j) == c2a[i].charAt(j + 1)) {
-                count++;
-            } else {
-                arr[i].push(count);
-                arr[i].push(c2a[i].charAt(j));
-                count = 1;
+    } else {
+        let arr = [];
+        count = 1;
+        for (let i = 1; i < n; i++) {
+            arr[i] = [];
+            for (let j = 0; j < c2a[i].length; j++) {
+                if (c2a[i].charAt(j) == c2a[i].charAt(j + 1)) {
+                    count++;
+                } else {
+                    arr[i].push(count);
+                    arr[i].push(c2a[i].charAt(j));
+                    count = 1;
+                }
             }
+        };
+        var result = "";
+        for (let i = 1; i < arr.length; i++) {
+            result += `${arr[i].join("")}\n`;
         }
-    };
-    var result = "";
-    for (let i = 1; i < arr.length; i++) {
-        result += `${arr[i].join("")}\n`;
-    }
 
-    $("#c2b").html(result);
-}
+        $("#c2b").html(result);
+    }
 };
 function c3(e) {
     e.preventDefault();
@@ -46,45 +46,45 @@ function c3(e) {
     n = c3Arr.length;
     console.log(parseInt(c3Arr[0]));
     console.log(c3Arr.length);
-    if(parseInt(c3Arr[0])!=c3Arr.length-1){
+    if (parseInt(c3Arr[0]) != c3Arr.length - 1) {
         $("#c3b").html("Du lieu khong dung");
-    }else{
+    } else {
         $("#c3b").html("dung");
-    for (let i = 1; i < n; i++) {
-        c3Arr[i] = c3Arr[i].split("");
-        var hs;
-        m = c3Arr[i].length;
-        for (j = 1; j < m; j++) {
-            if (j !== m - 1) {
-                console.log("j" + j);
-                hs = Math.pow(10, i);
-                if (parseInt(c3Arr[i].join("")) > hs) {
-                    console.log("so " + parseInt(c3Arr[i].join("")));
-                    console.log(parseInt(c3Arr[i][m - j]));
-                    if (parseInt(c3Arr[i][m - j]) >= 5) {
-                        c3Arr[i][m - j] = 0;
-                        c3Arr[i][m - j - 1] = parseInt(c3Arr[i][m - j - 1]) + 1;
+        for (let i = 1; i < n; i++) {
+            c3Arr[i] = c3Arr[i].split("");
+            var hs;
+            m = c3Arr[i].length;
+            for (j = 1; j < m; j++) {
+                if (j !== m - 1) {
+                    console.log("j" + j);
+                    hs = Math.pow(10, i);
+                    if (parseInt(c3Arr[i].join("")) > hs) {
+                        console.log("so " + parseInt(c3Arr[i].join("")));
+                        console.log(parseInt(c3Arr[i][m - j]));
+                        if (parseInt(c3Arr[i][m - j]) >= 5) {
+                            c3Arr[i][m - j] = 0;
+                            c3Arr[i][m - j - 1] = parseInt(c3Arr[i][m - j - 1]) + 1;
+                        } else {
+                            c3Arr[i][m - j] = 0;
+                            c3Arr[i][m - j - 1] = 0;
+                        }
                     } else {
-                        c3Arr[i][m - j] = 0;
-                        c3Arr[i][m - j - 1] = 0;
                     }
                 } else {
-                }
-            } else {
-                if (parseInt(c3Arr[i][1]) >= 5) {
-                    c3Arr[i][0] = parseInt(c3Arr[i][0]) + 1;
-                    c3Arr[i][1] = 0;
-                } else {
-                    c3Arr[i][1] = 0
+                    if (parseInt(c3Arr[i][1]) >= 5) {
+                        c3Arr[i][0] = parseInt(c3Arr[i][0]) + 1;
+                        c3Arr[i][1] = 0;
+                    } else {
+                        c3Arr[i][1] = 0
+                    }
                 }
             }
         }
+        for (let i = 1; i < c3Arr.length; i++) {
+            result += c3Arr[i].join("") + "\n";
+        }
+        $("#c3b").html(result);
     }
-    for (let i = 1; i < c3Arr.length; i++) {
-        result +=c3Arr[i].join("")+"\n";
-    }
-    $("#c3b").html(result);
-}
 
 }
 function c4(e) {
@@ -110,75 +110,77 @@ function c4(e) {
         }
         var y = ""
         result.forEach((x) => {
-            y +=x+"\n"
+            y += x + "\n"
         })
         $("#c4b").html(y);
     }
 }
-function c5(e){
+function c5(e) {
     e.preventDefault();
-    var c5a=$("#c5a").val();
-    var c5Arr=c5a.split("\n");
-    n=c5Arr.length;
-    var a=[];
-    var scl=c5Arr[0].split(" ");
-    var r=scl[0];
-    var c=scl[1];
-    var o=r*c;
-    var i=0;
-    while(i<o){
+    var c5a = $("#c5a").val();
+    var c5Arr = c5a.split("\n");
+    n = c5Arr.length;
+    var a = [];
+    var scl = c5Arr[0].split(" ");
+    var r = scl[0];
+    var c = scl[1];
+    var o = r * c;
+    var i = 0;
+    while (i < o) {
         a.push(0);
         i++
     }
-    for(let i=1;i<=r;i++){
-        for(let j=0;j<c;j++){
-            if(c5Arr[i].charAt(j)=="S"){
-                let x=(i-1)*c+j;
-                a[x]=1;
+    console.log(o);
+    console.log(a);
+    for (let i = 1; i <= r; i++) {
+        for (let j = 0; j < c; j++) {
+            if (c5Arr[i].charAt(j) == "S") {
+                let x = (i - 1) * c + j;
+                a[x] = 1;
             }
         }
     }
-    for(let i=0;i<r;i++){
-        let tam=0;
-        for(let j=0;j<c;j++){
-            let x=i*c+j;
-            if(a[x]==1){
+    console.log(a);
+    for (let i = 0; i < r; i++){
+        let tam = 0;
+        for (let j = 0; j < c; j++) {
+            let x = i * c + j;
+            if (a[x] == 1) {
                 tam++;
                 break;
             }
         }
-        if(tam==0){
-          for(let j=0;j<c;j++){
-             let x=i*c+j;   
-             a[x]=2;
-          }  
+        if (tam == 0) {
+            for (let j = 0; j < c; j++) {
+                let x = i * c + j;
+                a[x] = 2;
+            }
         }
 
     }
-    for(let i=0;i<c;i++){
-        let tam=0;
+        for(let i=0;i<c;i++){
+            let tam=0;
+            for(let j=0;j<r;j++){
+                let x=i+j*c;
+                if(a[x]==1){
+                    tam++
+                    break;
+            }    
+            /*if(tam==0){
+                for(let j=1;j<=r;j++){
+                    let x=i+j*4-r-1;
+                    a[x]=2;
+                }
+            }*/
+       }
+       if(tam==0){
         for(let j=0;j<r;j++){
-            let x=i+j*4;
-            if(a[x]==1){
-                tam++
-                //break;
-        }    
-        // console.log("tam "+tam);
-        // if(tam==0){
-        //     for(let j=1;j<=r;j++){
-        //         let x=i+j*4-r-1;
-        //         a[x]=2;
-        //     }
-        //     console.log('iii'+i);
-        // }
-   }
-   if(tam==0){
-    for(let j=0;j<r;j++){
-        let x=i+j*4;
-        a[x]=2;  
+            let x=i+j*c;
+            a[x]=2;  
+        }
     }
-}
-}
+    }
+    console.log(a);
     var count=0;
     a.forEach(x=>{
         if(x==2){
@@ -187,85 +189,85 @@ function c5(e){
     })
     $("#c5b").html(count);
 }
-function c6(e){
+function c6(e) {
     e.preventDefault();
-    var c6a=$("#c6a").val();
-    var c6Arr=c6a.split(" ");
-    if(c6Arr.length!=2){
+    var c6a = $("#c6a").val();
+    var c6Arr = c6a.split(" ");
+    if (c6Arr.length != 2) {
         $("#c6b").html("Du lieu khong dung");
-    }else{
-    var result=[];
-    var str="";
-    var x=c6Arr[1]%(c6Arr[0]-1);
-    var y=c6Arr[1]/(c6Arr[0]-1);
-    if(x==0){
-        result.push(eval(c6Arr[0]*y-1));
-        result.push(eval(c6Arr[0]*y));
-    }else{
-        result.push(eval(Math.floor(y)*c6Arr[0]+1));
-        result.push(parseInt(Math.ceil(y))-1+parseInt(c6Arr[1]));
+    } else {
+        var result = [];
+        var str = "";
+        var x = c6Arr[1] % (c6Arr[0] - 1);
+        var y = c6Arr[1] / (c6Arr[0] - 1);
+        if (x == 0) {
+            result.push(eval(c6Arr[0] * y - 1));
+            result.push(eval(c6Arr[0] * y));
+        } else {
+            result.push(eval(Math.floor(y) * c6Arr[0] + 1));
+            result.push(parseInt(Math.ceil(y)) - 1 + parseInt(c6Arr[1]));
+        }
+        for (let i = 0; i <= 1; i++) {
+            str += result[i] + "\n"
+        }
+        $("#c6b").html(str);
     }
-    for(let i=0;i<=1;i++){
-        str+=result[i]+"\n"
-    }
-    $("#c6b").html(str);
 }
-}
-function c7(e){
+function c7(e) {
     e.preventDefault();
-    var c7a=$("#c7a").val();
-    var c7Arr=c7a.split("\n");
-    var arr=[];
-    var result=""
-    console.log(c7Arr[0]*3+1);
+    var c7a = $("#c7a").val();
+    var c7Arr = c7a.split("\n");
+    var arr = [];
+    var result = ""
+    console.log(c7Arr[0] * 3 + 1);
     console.log(c7Arr.length);
-    if((c7Arr[0]*3+1)!=c7Arr.length){
+    if ((c7Arr[0] * 3 + 1) != c7Arr.length) {
         $("#c7b").html("Dữ liệu đầu vào không đúng");
-    }else{
-        for(let i=1;i<=c7Arr[0];i++){
+    } else {
+        for (let i = 1; i <= c7Arr[0]; i++) {
             console.log(i);
-            console.log(c7Arr[i*i+2]);
-            arr.push(c7Arr[i*i+2].split("").map(t=>{
-                for(let j=1;j<c7Arr[i*i+1];j++){
-                    console.log(c7Arr[i*i+1]);
-                    t+=t;
+            console.log(c7Arr[i * i + 2]);
+            arr.push(c7Arr[i * i + 2].split("").map(t => {
+                for (let j = 1; j < c7Arr[i * i + 1]; j++) {
+                    console.log(c7Arr[i * i + 1]);
+                    t += t;
                 }
                 return t
             }))
         }
-        for(let i=0;i<arr.length;i++){
-            result+=`${i+1}\n${arr[i].join("")}\n`
+        for (let i = 0; i < arr.length; i++) {
+            result += `${i + 1}\n${arr[i].join("")}\n`
         }
         $("#c7b").html(result);
     }
-    
+
 }
-function c8(e){
+function c8(e) {
     e.preventDefault();
-    var c8a=$("#c8a").val();
-    if(isNaN(c8a)){
+    var c8a = $("#c8a").val();
+    if (isNaN(c8a)) {
         $("#c8b").html("du lieu khong dung")
-    }else{
-    var str="moo";
-    for(i=1;i<c8a;i++){
-        var o="";
-        for(j=1;j<=i;j++){
-            o+="o";
+    } else {
+        var str = "moo";
+        for (i = 1; i < c8a; i++) {
+            var o = "";
+            for (j = 1; j <= i; j++) {
+                o += "o";
+            }
+            o += "oo";
+            str = str + "m" + o + str;
         }
-        o+="oo";
-        str=str+"m"+o+str;
+        $("#c8b").html(str.charAt(c8a - 1));
     }
-    $("#c8b").html(str.charAt(c8a-1));
-}
 }
 function c9(e) {
     e.preventDefault();
     var x = [];
-    var result="";
+    var result = "";
     var c9a = $("#c9a").val().split("\n");
-    if(isNaN(c9a[0])){
+    if (isNaN(c9a[0])) {
         $("#c9b").html("Du lieu khong dung")
-    }else {
+    } else {
         for (i = 0; i < c9a[1].split(" ").length; i++) {
             console.log(i);
             var count = 0;;
@@ -281,8 +283,8 @@ function c9(e) {
                 x.push("NO");
             }
         }
-        x.forEach(y=>{
-            result+=y+"\n"
+        x.forEach(y => {
+            result += y + "\n"
         })
         $("#c9b").html(result);
     }
